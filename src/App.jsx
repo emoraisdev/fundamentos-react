434e2280c3820a143becb7a946383d2732056dfe
emoraisdev <evertonsmorais@gmail.com>
2024-08-26 21:36:01 -0300

    Criando Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
-import ComParametro from './components/basicos/ComParametro'
-import Fragmento from './components/basicos/Fragmento'
-import Primeiro from './components/basicos/Primeiro'
-import Aleatorio from './components/basicos/Aleatorio'
+import React from "react";
+import ComParametro from "./components/basicos/ComParametro";
+import Fragmento from "./components/basicos/Fragmento";
+import Primeiro from "./components/basicos/Primeiro";
+import Aleatorio from "./components/basicos/Aleatorio";
+import Card from "./components/layout/Card";
+import "./App.css"
 
-export default function App(props){
+export default function App(props) {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
 
-    return (
-        <div>
-            <h1>Fundamentos React</h1>
-            <Aleatorio min={1} max={60}/>
-            <Fragmento/>
-            <ComParametro
-                titulo="Segundo Componente"
-                subtitulo="Muito legal!"/>
-            <Primeiro/>
-        </div>
-    )
+      <div className="Cards">
+        <Card titulo="#01 - Primeiro">
+          <Primeiro />
+        </Card>
+        <Card titulo="#02 - Desafio Aleatório">
+          <Aleatorio min={1} max={60} />
+        </Card>
+        <Card titulo="#03 - Fragmento">
+          <Fragmento />
+        </Card>
+        <Card titulo="#04 - Com Parâmetro">
+          <ComParametro titulo="Segundo Componente" subtitulo="Muito legal!" />
+        </Card>
+      </div>
+    </div>
+  );
 }
